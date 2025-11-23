fed7f79d4619781137c49f6314ecd521f253fae4
Joel Ting <joelccting@gmail.com>
2025-11-23 17:47:08 +0800

    modified:   fibonacci/main.c

--- fibonacci/main.c
+++ fibonacci/main.c
@@ -6,54 +6,106 @@ const ULL m = 1000000007;
 
 void print(ULL *a)
 {
-    printf("+ %llu, %llu\n", a[2], a[1]);
-    printf("- %llu, %llu\n", a[1], a[0]);
+    printf("| %llu, %llu |\n", a[1] + a[0], a[1]);
+    printf("| %llu, %llu |\n", a[1], a[0]);
 }
 
-void fib(ULL *u, ULL k)
+/** F(2k) = F(k)* (2*F(k-1)+F(k))
+ */
+void compute(ULL *u)
 {
+    ULL tmp[2];
+
+//    tmp[1] = (u[1] * u[1] + u[0] * u[0]) % m;
+    tmp[1] = u[1] * u[1];
+    tmp[1] += u[0] * u[0];
+    tmp[1] %= m;
+//    tmp[0] = u[0] * (2 * u[1] - u[0]) % m;
+    tmp[0] = u[0] * (2 * u[1] - u[0]);
+    tmp[0] %= m;
+
+    u[1] = tmp[1];
+    u[0] = tmp[0];
+}
+
+void fib2(ULL *u, ULL k)
+{
+    ULL tmp;
+
     if (k == 0)
     {
+        u[0] = 0;
+        u[1] = 1;
         return;
     }
 
-    if (k == 1)
-    {
-         u[0] = u[1] % m;
-         u[1] = u[2] % m;
-         u[2] = (u[1] % m + u[0] % m) % m;
-         return;
-    }
+    fib2(u, k / 2);
+    compute(u);
 
-    if (k % 2 == 0)
+    if (k % 2) //odd
     {
-        fib(u, k / 2);
-        fib(u, k / 2);
-    }
-    else //odd
-    {
-        fib(u, (k - 1) / 2);
-        fib(u, (k - 1) / 2);
-        fib(u, 1);
+        tmp = u[0];
+        u[0] = u[1];
+        u[1] = tmp + u[1];
     }
 }
+
+#if 1
+void fib_fast_doubling(ULL k, ULL *f_n, ULL *f_n1)
+{
+    if (k == 0)
+    {
+        *f_n = 0;
+        *f_n1 = 1;
+        return;
+    }
+
+    ULL a, b;
+    fib_fast_doubling(k >> 1, &a, &b);
+
+    ULL c = a * (2 * b - a); c %= m;
+    ULL d = a * a + b * b; d %= m;
+
+    if (k & 1)
+    {
+        *f_n = d;
+        *f_n1 = c + d;
+    }
+    else
+    {
+        *f_n = c;
+        *f_n1 = d;
+    }
+}
+
+ULL fib(ULL k)
+{
+    //ULL fn, fn1;
+    //fib_fast_doubling(k, &fn, &fn1);
+
+    ULL a[2];
+    fib_fast_doubling(k, &(a[0]), &(a[1]));
+    //print(a);
+    return a[0];
+}
+#endif
 
 int main()
 {
     ULL n;
     scanf("%llu", &n);
-    ULL a[3] = {0, 1, 1};
+#if 0
+    ULL a[2];
 
-    if (n == 0)
-    {
-        printf("0");
+    for (int i = 1; i < 25; ++i) {
+        printf("---%d---\n", i);
+        n = i;
+        fib2(a, n);
+        //printf("%llu", a[0]);
+        print(a);
     }
-    else
-    {
-        fib(a, n);
-        //print(a);
-        printf("%llu", a[0]);
-    }
+#endif
+    printf("%llu", fib(n));
 
     return 0;
 }
