8404db3c4df811920fd2e3a23326db2c7b83e662
Joel Ting <joelccting@gmail.com>
2025-12-05 14:12:00 +0800

    CORRECT 	modified:   2330/main.cpp

--- 2330/main.cpp
+++ 2330/main.cpp
@@ -13,11 +13,11 @@ int main()
 
     for (int i = 0; i < n; ++i)
     {
-        for (int j = 0; j < n; ++j)
+        for (int j = i + 1; j < n; ++j)
         {
-            for (int k = 0; k < n; ++k)
+            for (int k = j + 1; k < n; ++k)
             {
-                if ((i == j) || (j == k) || (i == k)) continue;
+                //if ((i == j) || (j == k) || (i == k)) continue;
 
                 if ((a[i] + a[j] + a[k]) % 10 == 0)
                 {
