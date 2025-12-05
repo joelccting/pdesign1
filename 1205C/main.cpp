ddd53edf11923bdfbd64d42899d1f7e247cd5b6e
Joel Ting <joelccting@gmail.com>
2025-12-05 16:11:04 +0800

    1205C. CORRECT. 	new file:   1205C/main.cpp

---
+++ 1205C/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; ++i) arr.push_back(i + 1);
+    //for (int i = 0; i < n; ++i) cout << arr[i] << endl;
+
+    do {
+
+        for (int x : arr) cout << x << " ";
+        cout << "\n";
+    } while (next_permutation(arr.begin(), arr.end()));
+
+    return 0;
+}
