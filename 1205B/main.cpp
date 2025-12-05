55cec6c34088bf7e4258b6860538e35c4b7372dd
Joel Ting <joelccting@gmail.com>
2025-12-05 16:09:38 +0800

    Add 1205B. CORRECT. 	new file:   1205B/main.cpp

---
+++ 1205B/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+typedef unsigned long long ULL;
+
+int main()
+{
+    int n, x, cnt = 0;
+    cin >> n >> x;
+
+    ULL a[n];
+
+    for (int i = 0; i < n; ++i) cin >> a[i];
+
+    for (int mask = 0; mask <= (1 << n) - 1; ++mask)
+    {
+        ULL sum = 0;
+        for (int i = 0; i <= n - 1; ++i)
+        {
+            if (mask & (1 << i))
+            {
+                sum += a[i];
+            }
+        }
+
+        if (x == sum) ++cnt;
+    }
+
+    cout << cnt << endl;
+
+    return 0;
+}
